d1129a9b9141a3e5b5f6c5a34a70b7b607b2ac22
Frac7 <francescacella96@gmail.com>
2017-05-23 19:22:30 +0200

    Prova cancella

--- Progetto/src/java/mod/Profilo.java
+++ Progetto/src/java/mod/Profilo.java
@@ -57,7 +57,7 @@ public class Profilo extends HttpServlet {
                         if(PostFactory.getInstance().deleteAllByUser(u) == true)
                             if(UtentiRegistratiFactory.getInstance().deleteUser(u) == true)
                             {
-                                session.invalidate();
+                                request.setAttribute("cancella",1);
                                 request.getRequestDispatcher("login.jsp").forward(request, response);
                             }
                 }
